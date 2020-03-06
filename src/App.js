12a272f3c720710568c0d6ba84947d08dd7c36b5
EugeneShatalin <shatalin_ei@mail.ru>
2020-03-06 11:45:42 +0600

    I completed the 11th lesson "The Way of the Samurai". Layout layout on grid.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
 
 const App = () => {
   return (
-      <div>
-         <Header />
-         <Technologies/>
+      <div className='app-wrapper'>
+          <header className='header'>
+              <img src="./img/logo.png" alt="logo" />
+          </header>
+          <nav className='nav'>
+              <div>
+                  <a>Profile</a>
+              </div>
+              <div>
+                  <a>Message</a>
+              </div>
+              <div>
+                  <a>News</a>
+              </div>
+              <div>
+                  <a>Music</a>
+              </div>
+              <div>
+                  <a>Settings</a>
+              </div>
+          </nav>
+          <div className='content'>
+              <div>
+                  <img src="./img/cosmos.jpg" alt="cosmos"/>
+              </div>
+              <div>
+                  ava + description
+              </div>
+              <div>
+                  my posts
+                  <div>
+                      new post
+                  </div>
+              </div>
+              <div>
+                  <div>post 1</div>
+                  <div>post 2</div>
+              </div>
+          </div>
       </div>
+
+
   );
 }
 
