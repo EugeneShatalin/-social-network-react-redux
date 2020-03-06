23b0112cfa552f5e42f48395228a64502bcfa453
EugeneShatalin <shatalin_ei@mail.ru>
2020-03-07 00:17:08 +0600

    I completed the 12th lesson React JS course "The Way of the Samurai". Component breakdown.

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src="./img/logo.png" alt="logo" />
+    </header>
+}
+
+export  default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Profile = () => {
+    return  <div className='content'>
+        <div>
+            <img src="./img/cosmos.jpg" alt="cosmos"/>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            my posts
+            <div>
+                new post
+            </div>
+        </div>
+        <div>
+            <div>post 1</div>
+            <div>post 2</div>
+        </div>
+    </div>
+}
+
+export  default Profile;
