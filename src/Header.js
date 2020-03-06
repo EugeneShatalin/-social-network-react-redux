d5ed42d39716557b9dce7704e0ea096f0bb3a4a4
EugeneShatalin <shatalin_ei@mail.ru>
2020-03-06 20:24:29 +0600

    I completed the 9th lesson "The Way of the Samurai".

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <a href="#g">css   </a>
+            <a href="#g">html  </a>
+            <a href="#g">js  </a>
+        </div>
+    );
+}
+
+export default Header;
