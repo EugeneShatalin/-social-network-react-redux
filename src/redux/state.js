883571ffe63dfba5e75237b4387dcc7bb8b2bbac
EugeneShatalin <shatalin_ei@mail.ru>
2020-03-22 17:50:17 +0600

    I completed the 33 lesson React JS course "The Way of the Samurai". Made adding a post to the wall.

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,5 @@
+import {rerenderEntireTree} from "../render";
+
 let state = {
     profilePage: {
         posts: [
@@ -33,6 +35,7 @@ export let addPost = (postMessage) => {
         likeCount: 0
     };
     state.profilePage.posts.push(newPost);
+    rerenderEntireTree(state);
 };
 
 export default state;
